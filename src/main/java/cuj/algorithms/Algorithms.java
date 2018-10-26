b6a24c06ea356890eba66f7c01c1076d2fbe9599
cujamin <chbnjamin@gmail.com>
2018-10-26 16:56:01 +0800

    第一次提交

---
+++ src/main/java/cuj/algorithms/Algorithms.java
@@ -0,0 +1,14 @@
+package cuj.algorithms;
+
+import cuj.algorithms.bitonic.serial.Bitonic;
+
+/**
+ * @Auther: cujamin
+ * @Date: 2018/10/26 16:41
+ * @Description:
+ */
+public class Algorithms {
+    public static void main(String[] args) {
+        new Bitonic().start();
+    }
+}
---
+++ src/main/java/cuj/algorithms/bitonic/serial/Bitonic.java
@@ -0,0 +1,37 @@
+package cuj.algorithms.bitonic.serial;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Auther: cujamin
+ * @Date: 2018/10/26 16:42
+ * @Description:
+ */
+public class Bitonic {
+    private static Logger logger = LoggerFactory.getLogger(Bitonic.class);
+    public void start(){
+        logger.info("bitonic start");
+    }
+
+    private void sortUp(){
+
+    }
+
+    private void sortDown(){
+
+    }
+
+    private void mergeUp(){
+
+    }
+
+    private void mergeDown()
+    {
+
+    }
+
+    private void exchange(){
+
+    }
+}
